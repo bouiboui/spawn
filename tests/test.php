b2bdcf000c5ba7583a415aee9fca205740ab7470
julien <julienbouisset@exagroup.biz>
2016-05-20 00:39:59 +0200

    Simulate http request

--- tests/test.php
+++ tests/test.php
@@ -1,5 +1,16 @@
 <?php
 
-echo is_file($argv[1]) ? file_get_contents($argv[1]) : var_export($argv, true);
+if (isset($argv)) {
 
+    // For direct/process tests
+    echo is_file($argv[1]) ? file_get_contents($argv[1]) : var_export($argv, true);
+
+} else {
+
+    // For curl/http tests
+    echo var_export($_REQUEST, true);
+
+}
+
+// Simulate latency
 sleep(mt_rand(0, 2));
