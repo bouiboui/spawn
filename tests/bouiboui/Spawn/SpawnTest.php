d60bea6e179947636c4264675634f011e4e51311
Julien <julienbouisset@exagroup.biz>
2016-05-19 18:00:50 +0200

    Updated tests

--- tests/bouiboui/Spawn/SpawnTest.php
+++ tests/bouiboui/Spawn/SpawnTest.php
@@ -7,36 +7,33 @@ class SpawnTest extends \PHPUnit_Framework_TestCase
 {
     public function testAddProcessFromDirectory()
     {
+        $this->assertCommandCount('test ' . dirname(dirname(__DIR__)) . '/data', 3);
+    }
+
+    private function assertCommandCount($command, $count)
+    {
         $spawn = new Spawn();
-        $spawn->addProcessesFromDirectory('php tests/test.php', dirname(dirname(__DIR__)). '/data');
-        static::assertEquals(3, $spawn->getProcessesCount());
+        $spawn->addProcessesFromCommand(explode(' ', $command));
+        static::assertEquals($count, $spawn->getProcessesCount());
     }
 
     public function testAddProcessWithSingleRange()
     {
-        $spawn = new Spawn();
-        $spawn->addProcessesFromCommand('php tests/test.php', 'arg{0-0}');
-        static::assertEquals(1, $spawn->getProcessesCount());
+        $this->assertCommandCount('test arg{0-0}', 1);
     }
 
     public function testAddProcessWithMultipleRange()
     {
-        $spawn = new Spawn();
-        $spawn->addProcessesFromCommand('php tests/test.php', 'arg{1-10}');
-        static::assertEquals(10, $spawn->getProcessesCount());
+        $this->assertCommandCount('test arg{1-10}', 10);
     }
 
     public function testAddProcessWithoutRange()
     {
-        $spawn = new Spawn();
-        $spawn->addProcessesFromCommand('php tests/test.php', 'arg1');
-        static::assertEquals(1, $spawn->getProcessesCount());
+        $this->assertCommandCount('test arg1', 1);
     }
 
     public function testAddSingleProcess()
     {
-        $spawn = new Spawn();
-        $spawn->addSingleProcess('php tests/test.php');
-        static::assertEquals(1, $spawn->getProcessesCount());
+        $this->assertCommandCount('test', 1);
     }
 }
